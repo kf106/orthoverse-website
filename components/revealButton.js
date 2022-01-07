3bee03dbd6e4eb7f63a267dbe909f84b576902d8
Keir <keir@chainfrog.com>
2022-01-07 19:33:39 +0200

    next install, extra files

---
+++ components/revealButton.js
@@ -0,0 +1,30 @@
+// components/mintButton.js
+import Image from 'next/image';
+import { Typography, Button } from '@material-ui/core'
+import ortho from '../public/ortho.svg';
+
+const stateText = {
+  "hidden": "Reveal your NFT",
+  "revealing": "Revealing...",
+  "revealed": "Congratulations!",
+  "rejected": "User rejected tx",
+  "insufficient": "Address has insufficient funds"
+}
+
+function RevealButton( { state } ) {
+
+  return (
+    <Button className="w-64 h-10"
+      disableElevation
+      variant='contained'
+      color='secondary'
+    >
+      <div><Image src={ ortho } alt="Reveal NFT" height="30px" width="30px"/></div>
+      <Typography variant='button'>
+        { stateText[state] }
+      </Typography>
+    </Button>
+  );
+};
+
+export default RevealButton;
